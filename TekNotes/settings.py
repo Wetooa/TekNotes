94bbf392af2a936419f044113ce961e8b7f746da
wetooa <derpykidyt@gmail.com>
2024-10-01 14:22:10 +0800

    decided on using tailwind

--- TekNotes/settings.py
+++ TekNotes/settings.py
@@ -126,6 +126,13 @@ STATIC_ROOT = BASE_DIR / "static"
 CKEDITOR_BASEPATH = "static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
+
+# Tailwind settings
+# TAILWIND_APP_NAME = "teknotes"
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
