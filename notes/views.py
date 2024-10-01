e436b93c452b72fede6a567f43c4aab06d8f431b
wetooa <derpykidyt@gmail.com>
2024-10-01 16:07:20 +0800

    improved ui

--- notes/views.py
+++ notes/views.py
@@ -4,9 +4,59 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        "home_page.html",
+        "home.html",
         {
             "notes": [
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
                 {
                     "title": "The Future of Robotics",
                     "creator": "Simon Escano",
