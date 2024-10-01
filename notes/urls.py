c3c114dff91bc8b5dfaf4d10d6d60a4eb138f497
wetooa <derpykidyt@gmail.com>
2024-10-01 16:47:41 +0800

    mistake, should be notes branch

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("your-notes", views.tek_a_note, name="your-notes"),
+    path("likes", views.tek_a_note, name="likes"),
     path("tek-a-note", views.tek_a_note, name="tek-a-note"),
 ]
