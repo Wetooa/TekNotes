25cb0b0986fbdaff42beb345ae12a1ea2281da19
wetooa <derpykidyt@gmail.com>
2024-10-01 15:28:33 +0800

    started on basic note templates

--- TekNotes/settings.py
+++ TekNotes/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 # Ckeditor settings
--- notes/views.py
+++ notes/views.py
@@ -2,4 +2,26 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "home_page.html")
+    return render(
+        request,
+        "home_page.html",
+        {
+            "notes": [
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+                {
+                    "title": "The Future of Robotics",
+                    "creator": "Simon Escano",
+                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
+                },
+            ]
+        },
+    )
