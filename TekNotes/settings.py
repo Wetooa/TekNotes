6338713132240869ad3de6eb7294e13144060f05
Simon Escano <escanosimonlyster@gmail.com>
2024-10-17 21:36:51 +0800

    Added media root directory

--- TekNotes/settings.py
+++ TekNotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Media directory
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Ckeditor settings
 CKEDITOR_BASEPATH = "static/ckeditor/ckeditor/"
