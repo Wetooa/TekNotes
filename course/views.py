f2522358b26321bf70c9a9120cf59a40bd76f2b2
Simon Escano <escanosimonlyster@gmail.com>
2024-10-17 19:52:57 +0800

    Implemented delete Course function

--- course/views.py
+++ course/views.py
@@ -18,3 +18,12 @@ def add_a_course(request):
         form = CourseForm()
 
     return render(request, 'course/add_a_course.html', {'form': form})
+
+@login_required
+def delete_course(request, course_id):
+    try:
+        course = Course.objects.get(id=course_id, created_by=request.user)
+        course.delete()
+        return HttpResponseRedirect('/')
+    except Course.DoesNotExist:
+        return HttpResponseRedirect('/course/not_found/')
