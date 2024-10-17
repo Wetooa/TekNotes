574ead7c6e4d71d2474dd5fad88b89342710cfa4
Simon Escano <escanosimonlyster@gmail.com>
2024-10-17 19:49:33 +0800

    Implemented Add a Course function

--- course/views.py
+++ course/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from course.models import Course
+from .forms import CourseForm
+
+@login_required
+def add_a_course(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.created_by = request.user
+            course.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = CourseForm()
+
+    return render(request, 'course/add_a_course.html', {'form': form})
