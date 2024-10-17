d0277cb2a080e64fdb0e70a52287271af81a1bc6
Simon Escano <escanosimonlyster@gmail.com>
2024-10-17 21:17:37 +0800

    Created view for index

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import OuterRef, Subquery
+from notes.models import Note
+from course.models import Course
+from tags.models import Tag
+from comments.models import Like
 
-# Create your views here.
+def index(request):
+    notes = Note.objects.filter(is_archived=False, is_private=False).order_by('-modified_at')
+    courses = Course.objects.filter(note__in=notes).distinct()
+    tags = Tag.objects.filter(notes__in=notes).distinct()
+
+    return render(request, 'core/index.html', {
+        'notes': notes,
+        'courses': courses,
+        'tags': tags
+    })
+
