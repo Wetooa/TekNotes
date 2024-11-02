29732968004c1f251fc83ea517120e3ed7861e05
wetooa <derpykidyt@gmail.com>
2024-11-02 15:30:45 +0800

    merging notes

--- notes/views.py
+++ notes/views.py
@@ -1,43 +1,70 @@
 from django.shortcuts import render
-
-
-def index(request):
-    return render(
-        request,
-        "home.html",
-        {
-            "notes": [
-                {
-                    "title": "The Future of Robotics",
-                    "creator": "Simon Escano",
-                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
-                },
-                {
-                    "title": "The Future of Robotics",
-                    "creator": "Simon Escano",
-                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
-                },
-                {
-                    "title": "The Future of Robotics",
-                    "creator": "Simon Escano",
-                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
-                },
-                {
-                    "title": "The Future of Robotics",
-                    "creator": "Simon Escano",
-                    "content": "Robots are becoming integral in various industries, from manufacturing to healthcare.",
-                },
-            ],
-            "tags": ["Robotics", "AI", "Machine Learning"],
-        },
-    )
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from .forms import NoteForm
+from tags.models import Tag
+from .models import Note
+from django.http import HttpResponseRedirect
 
 
 def tek_a_note(request):
     if request.method == "POST":
-        title = request.POST.get("title")
-        creator = request.POST.get("creator")
-        content = request.POST.get("content")
-        print(title, creator, content)
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.created_by = request.user
 
-    return render(request, "tek_a_note.html")
+            note.save()
+
+            tag_input = request.POST.get("tags", "")
+            if tag_input:
+                tag_names = [tag.strip() for tag in tag_input.split(",")]
+                for tag_name in tag_names:
+                    if tag_name:
+                        tag, created = Tag.objects.get_or_create(name=tag_name)
+                        note.tags.add(tag)
+
+            return HttpResponseRedirect("/notebook/")
+    else:
+        form = NoteForm()
+
+    return render(request, "notes/tek_a_note.html", {"form": form})
+
+
+def note_detail(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    return render(request, "notes/note_detail.html", {"note": note})
+
+
+@login_required
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id, created_by=request.user)
+    if request.method == "POST":
+        note.delete()
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
+
+
+@login_required
+def archive_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id, created_by=request.user)
+
+    if request.method == "POST":
+        note.is_archived = not note.is_archived
+        note.save()
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
+
+
+@login_required
+def hide_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id, created_by=request.user)
+
+    if request.method == "POST":
+        note.is_private = not note.is_private
+        note.save()
+        return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
