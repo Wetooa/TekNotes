d429ec0d39660c25c7390cb56c5237d784710b06
wetooa <derpykidyt@gmail.com>
2024-11-06 15:26:23 +0800

    comitting

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.dispatch import receiver
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as log_out
 from django.db.models import OuterRef, Subquery
@@ -36,34 +37,48 @@ def logout(request):
 def profile(request, user_id):
     profile = Profile.objects.get(user__id=user_id)
     notes = Note.objects.filter(created_by=user_id, is_private=False)
-    
-    return render(request, 'authentication/profile.html', {
-        'profile': profile,
-        'notes': notes,
-    })
 
-@receiver(user_signed_up)
-def create_profile_on_google_signup(request, user, **kwargs):
-    Profile.objects.create(user=user)
+    return render(
+        request,
+        "authentication/profile.html",
+        {
+            "profile": profile,
+            "notes": notes,
+        },
+    )
+
+
+# @receiver(user_signed_up)
+# def create_profile_on_google_signup(request, user, **kwargs):
+#     Profile.objects.create(user=user)
 
 
 def profile_likes(request, user_id):
     profile = Profile.objects.get(user__id=user_id)
-    latest_likes = Like.objects.filter(note=OuterRef('pk'), user__id=user_id).order_by('-created_at')
-    notes = Note.objects.filter(likes__user__id=user_id, is_private=False).annotate(
-        like_created_at=Subquery(latest_likes.values('created_at')[:1])
-    ).distinct().order_by('-like_created_at')
-    
-    return render(request, 'authentication/profile.html', {
-        'profile': profile,
-        'notes': notes
-    })
+    latest_likes = Like.objects.filter(note=OuterRef("pk"), user__id=user_id).order_by(
+        "-created_at"
+    )
+    notes = (
+        Note.objects.filter(likes__user__id=user_id, is_private=False)
+        .annotate(like_created_at=Subquery(latest_likes.values("created_at")[:1]))
+        .distinct()
+        .order_by("-like_created_at")
+    )
+
+    return render(
+        request, "authentication/profile.html", {"profile": profile, "notes": notes}
+    )
+
 
 def profile_courses(request, user_id):
     profile = Profile.objects.get(user__id=user_id)
     courses = Course.objects.filter(created_by__id=user_id).distinct()
-    
-    return render(request, 'authentication/profile.html', {
-        'profile': profile,
-        'courses': courses,
-    })
+
+    return render(
+        request,
+        "authentication/profile.html",
+        {
+            "profile": profile,
+            "courses": courses,
+        },
+    )
