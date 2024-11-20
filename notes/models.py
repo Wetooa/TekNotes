8998819efeb91a6cc3b86e559eb50fada23098ac
wetooa <derpykidyt@gmail.com>
2024-11-20 23:12:32 +0800

    used djhtml to format htmldjango files

--- notes/models.py
+++ notes/models.py
@@ -1,10 +1,10 @@
-from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
+
 from course.models import Course
 from tags.models import Tag
-
-from django_ckeditor_5.fields import CKEditor5Field
 
 
 class Note(models.Model):
