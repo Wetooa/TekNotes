ab742e015bac8f557d1d64fbb880df06951871ba
wetooa <derpykidyt@gmail.com>
2024-11-28 17:25:49 +0800

    fixed quick database exposure

--- TekNotes/settings.py
+++ TekNotes/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,11 +121,11 @@ WSGI_APPLICATION = "TekNotes.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "teknotes_db",
-        "USER": "teknotes_user",
-        "PASSWORD": "123456789",
-        "HOST": "wetooa.me",
-        "PORT": "8002",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
