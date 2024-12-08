0d1e2428e7ed8231212f4fb2550dd088e8388efd
simonescano <escanosimonlyster@gmail.com>
2024-12-09 07:21:19 +0800

    Converted highlight.css into inline tailwindcss

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -26,11 +26,11 @@ def highlight(text, query, style):
     def get_replacement(match):
         match_text = match.group(0)
         if style == 'note_title':
-            return f'<span class="highlight highlight_note_title">{match_text}</span>'
+            return f'<span class="bg-yellow-300">{match_text}</span>'
         elif style == 'user':
-            return f'<span class="highlight highlight_user">{match_text}</span>'
+            return f'<span class="font-medium bg-yellow-300">{match_text}</span>'
         elif style == 'note_detail':
-            return f'<span class="highlight highlight_note_detail">{match_text}</span>'
+            return f'<span class="bg-yellow-300">{match_text}</span>'
         else:
             return match_text  
     
