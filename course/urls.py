568d65faf73f372eb03cf71ceff31d2c969ba368
simonescano <escanosimonlyster@gmail.com>
2024-12-09 20:57:55 +0800

    Added Edit Course

--- course/urls.py
+++ course/urls.py
@@ -6,4 +6,5 @@ app_name = 'course'
 urlpatterns = [
     path("add_a_course/", views.add_a_course, name="add_a_course"),
     path("delete_course/<int:course_id>/", views.delete_course, name="delete_course"),
+    path("edit_course/<int:course_id>/", views.edit_course, name="edit_course"),
 ]
--- course/views.py
+++ course/views.py
@@ -28,4 +28,24 @@ def delete_course(request, course_id):
         course.delete()
         return HttpResponseRedirect("/")
     except Course.DoesNotExist:
-        return HttpResponseRedirect("/course/not_found/")
+        return render(request, "course/course_missing.html", {"course_id": course_id})
+    
+from django.contrib import messages
+
+@login_required
+def edit_course(request, course_id):
+    try:
+        course = Course.objects.get(id=course_id, created_by=request.user)
+    except Course.DoesNotExist:
+        return render(request, "course/course_missing.html", {"course_id": course_id})
+
+    if request.method == "POST":
+        form = CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            course.code = request.POST.get('code')
+            course.description = request.POST.get('description')
+            form.save()
+            return HttpResponseRedirect(f"/search/course_{course.id}/")
+    else:
+        form = CourseForm(instance=course)
+    return render(request, "course/edit_course.html", {"form": form, "course": course})
